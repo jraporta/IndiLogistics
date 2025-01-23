db0ff38cf59136cd0b145bc2200d27e352f01986
jraporta <jraporta@gmail.com>
2025-01-23 20:45:54 +0100

    fix error message format

--- src/main/java/com/hackathon/inditex/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/hackathon/inditex/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.hackathon.inditex.Exceptions;
 
+import com.hackathon.inditex.DTO.MessageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidCenterCreationDataException.class)
-    public ResponseEntity<String> InvalidCenterDataHandler(InvalidCenterCreationDataException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    public ResponseEntity<MessageResponse> InvalidCenterDataHandler(InvalidCenterCreationDataException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(CenterNotExistsException.class)
-    public ResponseEntity<String> CenterNotExistsHandler(CenterNotExistsException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<MessageResponse> CenterNotExistsHandler(CenterNotExistsException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse(ex.getMessage()));
     }
 
 }
